097ec93761ea5ad861516f3144035954d2216d09
Eko Andri Subarnanto <eko.andri@icloud.com>
2018-10-05 16:48:12 +0700

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <React.Fragment className="App">
+        <GlobalStyle />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -20,9 +22,28 @@ class App extends Component {
             Learn React
           </a>
         </header>
-      </div>
+      </React.Fragment>
     );
   }
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+// import styled, { createGlobalStyle } from 'styled-components';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // <React.Fragment>
+    // {/* <GlobalStyle /> */}
+    <App />
+    // <p>Tes</p>
+    // <GlobalStyle/>
+  // </React.Fragment>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
+
+// const GlobalStyle = createGlobalStyle`
+//   html {
+//     margin: 0;
+//     padding: 0;
+//   }
+//   body {
+//     margin: 0;
+//     padding: 0;
+//     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+//       "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+//       sans-serif;
+//     -webkit-font-smoothing: antialiased;
+//     -moz-osx-font-smoothing: grayscale;
+//   }
+
+//   code {
+//     font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+//       monospace;
+//   }
+// `
