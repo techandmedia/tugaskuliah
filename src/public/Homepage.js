a5758ad23366db4a887b1417b94b53d0a8c9b20c
Eko Andri Subarnanto <eko.andri@icloud.com>
2018-10-06 21:37:36 +0700

    refactore config

--- src/public/Homepage.js
+++ src/public/Homepage.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
-import config from '../config/SiteConfig';
+import siteconfig from '../config/SiteConfig';
+import domainURL from '../config/Config';
 import img1 from '../img/school-1.jpg';
 
 const HomePage = () => {
   return (
     <React.Fragment>
       <Helmet>
-        <meta name="description" content={config.siteDescription} />
-        <title>{config.siteTitle}</title>
-        <link rel="canonical" href="http://localhost:3000" />
+        <meta name="description" content={siteconfig.siteDescription} />
+        <title>{siteconfig.siteTitle}</title>
+        <link rel="canonical" href={domainURL} />
       </Helmet>
       <h3>Home</h3>
       <Image src={img1} alt="tes" />
