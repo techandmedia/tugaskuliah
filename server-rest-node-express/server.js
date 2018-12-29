3cfce71968767ee5e10c6e21dc50e532977df5cb
Eko Andri Subarnanto <eko.andri@icloud.com>
2018-12-29 18:39:46 +0700

    Push

--- server-rest-node-express/server.js
+++ server-rest-node-express/server.js
@@ -11,10 +11,12 @@ const cors = require("cors");
 // const dosenRoute = require("./routes/route-dosen");
 // const mahasiswaRoute = require("./routes/route-mahasiswa");
 
-// New Route
+// Route for Siakad
 const getRoute = require("./routes/siakad/siakad-route-get");
 const postRoute = require("./routes/siakad/siakad-route-post");
 const signinRoute = require('./routes/siakad/siakad-route-signin-register')
+
+// Route for Pokemon
 const pokemonRoute = require('./routes/pokemon/pokemon-get')
 
 server.use(cors());
@@ -28,6 +30,13 @@ server.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 5001;
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
+// New Route
+server.use(getRoute);
+server.use(postRoute);
+server.use(signinRoute);
+server.use(pokemonRoute)
+
+
 // const baseURL = "/api/"
 // perhatikan pemakaian route, di sini apis nya adalah '/api/contacts', sedangkan di file route nya adalah '/'
 // server.use(contactRoute);
@@ -38,10 +47,5 @@ server.listen(port, () => console.log(`Listening on port ${port}`));
 // server.use(dosenRoute);
 // server.use(mahasiswaRoute);
 
-// New Route
-server.use(getRoute);
-server.use(postRoute);
-server.use(signinRoute);
-server.use(pokemonRoute)
 
 
